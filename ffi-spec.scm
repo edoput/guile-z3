33f5722eccc260bc73afba60bd74b88a19ddaeaa
Edoardo Putti <edoardo.putti@gmail.com>
2024-08-10 15:58:54 +0200

    [ffi-spec] move from syntax to values
    syntax must be macro-expanded while value need not. Guile's (system
    foreign) uses values so if we use macros we cannot interop as it is
    right now. As I want to mix binding from (system foreign) and (z3) I
    need to use their convetion.

--- ffi-spec.scm
+++ ffi-spec.scm
@@ -1,31 +1,21 @@
+;;; ffi-spec exposes aliases for '* to use in the foreign function
+;;; interface. It is intended to extend the bindings from guile's
+;;; own (system foreign) foreign types.
 (define-module (ffi-spec)
   #:use-module (system foreign)
   #:use-module (system foreign-object)
   #:use-module (system foreign-library)
-  #:export (z3:ffi
-	    ast
+  #:export (ast
 	    config
 	    context
 	    func-decl
-	    model))
+	    model
+	    sort))
 
-;;; this module defined syntax for writing down FFI spec for
-;;; Z3. This means you can write (z3:ffi ctx model) in place
-;;; of (list '* '*) whenever you are binding a foreign function.
-
-;;; As of now we also evaluated the ref-spec macro but this can
-;;; be bypassed with identifier-syntax so that other foreign types
-;;; can be used in the spec.
-
-(define-syntax z3:ffi
-  (syntax-rules (ctx model)
-    ((_ ref-spec ...)
-     (list (ref-spec) ...))))
-
-(define-syntax-rule (ast) "Z3 ast reference type" '*)
-(define-syntax-rule (config) "Z3 config reference type" '*)
-(define-syntax-rule (context) "Z3 context reference type" '*)
-(define-syntax-rule (func-decl) "Z3 function declaration reference type" '*)
-(define-syntax-rule (model) "Z3 model reference type" '*)
-
+(define ast '*)
+(define config '*)
+(define context '*)
+(define func-decl '*)
+(define model '*)
+(define sort '*)
 
--- reflect.scm
+++ reflect.scm
@@ -5,6 +5,7 @@
   #:use-module (context)
   #:use-module (model)
   #:use-module (ast)
+  #:use-module ((ffi-spec) #:renamer (symbol-prefix-proc 'z3:))
   #:export (model-num-consts
 	    model-num-funcs
 	    model-num-sorts
@@ -19,20 +20,17 @@
 (define z3-model-get-num-consts
   (foreign-library-function z3-lib "Z3_model_get_num_consts"
 			    #:return-type unsigned-int
-			    ;; ctx model
-			    #:arg-types '(* *)))
+			    #:arg-types (list z3:context z3:model)))
 
 (define z3-model-get-num-funcs
   (foreign-library-function z3-lib "Z3_model_get_num_funcs"
 			    #:return-type unsigned-int
-			    ;; ctx model
-			    #:arg-types '(* * *)))
+			    #:arg-types (list z3:context z3:model)))
 
 (define z3-model-get-num-sorts
   (foreign-library-function z3-lib "Z3_model_get_num_sorts"
 			    #:return-type unsigned-int
-			    ;; ctx model
-			    #:arg-types '(* *)))
+			    #:arg-types (list z3:context z3:model)))
 
 (define (model-num-consts ctx model)
   (z3-model-get-num-consts (unwrap-context ctx) (unwrap-model model)))
@@ -47,15 +45,13 @@
 (define z3-model-get-const-decl
   (foreign-library-function z3-lib "Z3_model_get_const_decl"
 			    #:return-type '*
-			    ;; ctx model
-			    #:arg-types (list '* '* unsigned-int)))
+			    #:arg-types (list z3:context z3:model unsigned-int)))
 
 
 (define z3-model-get-func-decl
   (foreign-library-function z3-lib "Z3_model_get_func_decl"
 			    #:return-type '*
-			    ;; ctx model unsigned-int
-			    #:arg-types (list '* '* unsigned-int)))
+			    #:arg-types (list z3:context z3:model unsigned-int)))
 
 (define (model-const-decl-ref ctx model i)
   (wrap-fun (z3-model-get-const-decl (unwrap-context ctx) (unwrap-model model) i)))
@@ -68,20 +64,17 @@
 (define z3-model-get-const-interp
   (foreign-library-function z3-lib "Z3_model_get_const_interp"
 			    #:return-type '*
-			    ;; ctx model func_decl
-			    #:arg-types '(* * *)))
+			    #:arg-types (list z3:context z3:model z3:func-decl)))
 
 (define z3-model-has-interp
   (foreign-library-function z3-lib "Z3_model_has_interp"
 			    #:return-type int
-			    ;; ctx model func_decl
-			    #:arg-types '(* * *)))
+			    #:arg-types (list z3:context z3:model z3:func-decl)))
 
 (define z3-model-get-func-interp
   (foreign-library-function z3-lib "Z3_model_get_func_interp"
 			    #:return-type '*
-			    ;; ctx model func_decl
-			    #:arg-types '(* * *)))
+			    #:arg-types (list z3:context z3:model z3:func-decl)))
 
 
 (define (model-has-interpretation? ctx model func)
@@ -92,14 +85,12 @@
 (define z3-model-get-sort
   (foreign-library-function z3-lib "Z3_model_get_sort"
 			    #:return-type '*
-			    ;; ctx model unsigned-int
-			    #:arg-types (list '* '* unsigned-int)))
+			    #:arg-types (list z3:context z3:model unsigned-int)))
 
 (define z3-model-get-sort-universe
   (foreign-library-function z3-lib "Z3_model_get_sort_universe"
 			    #:return-type '*
-			    ;; ctx model sort
-			    #:arg-types '(* * *)))
+			    #:arg-types (list z3:context z3:model z3:sort)))
 
 (define (model-sort-ref ctx model i)
   (wrap-sort (z3-model-get-sort (unwrap-context ctx) (unwrap-model model) i)))
