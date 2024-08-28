045335e8cb6702029bb87383c6d0cd9072b8f6dc
Edoardo Putti <edoardo.putti@gmail.com>
2024-08-28 16:07:53 +0200

    switch to ffi-spec in ast module

--- ast.scm
+++ ast.scm
@@ -2,6 +2,7 @@
   #:use-module (system foreign)
   #:use-module (system foreign-object)
   #:use-module (system foreign-library)
+  #:use-module ((ffi-spec) #:renamer (symbol-prefix-proc 'z3:))
   #:use-module (context)
   #:export (wrap-sort
 	    unwrap-sort
@@ -27,15 +28,15 @@
   (lambda (s p)
     (format p "#<sort ~a>" (pointer-address (unwrap-sort s)))))
 
-(define (sort->string ctx sort)
-  (z3-sort-to-string (unwrap-context ctx)
-		     (unwrap-sort sort)))
-
 (define z3-sort-to-string
   (foreign-library-function z3-lib "Z3_sort_to_string"
-			    #:return-type '*
-			    ;; ctx sort
-			    #:arg-types '(* *)))
+			    #:return-type z3:ast
+			    #:arg-types (list z3:context z3:sort)))
+
+(define (sort->string ctx sort)
+  (pointer->string
+   (z3-sort-to-string (unwrap-context ctx)
+		      (unwrap-sort sort))))
 
 ;;; AST 
 (define-wrapped-pointer-type <ast>
@@ -46,9 +47,8 @@
 
 (define z3-ast-to-string
   (foreign-library-function z3-lib "Z3_ast_to_string"
-			    #:return-type '*
-			    ;; ctx ast
-			    #:arg-types '(* *)))
+			    #:return-type z3:string
+			    #:arg-types (list z3:context z3:ast)))
 
 (define (ast->string ctx ast)
   "NOTE: string bytes are owned by context!"
@@ -65,52 +65,53 @@
 
 (define z3-mk-int-sort
   (foreign-library-function z3-lib "Z3_mk_int_sort"
-			    #:return-type '*
-			    #:arg-types '(*)))
+			    #:return-type z3:sort
+			    #:arg-types (list z3:context)))
 (define z3-mk-bool-sort
   (foreign-library-function z3-lib "Z3_mk_bool_sort"
-			    #:return-type '*
-			    #:arg-types '(*)))
+			    #:return-type z3:sort
+			    #:arg-types (list z3:context)))
 
 
 (define z3-get-sort-kind
   (foreign-library-function z3-lib "Z3_get_sort_kind"
 			    #:return-type int
 			    ;; ctx sort
-			    #:arg-types '(* *)))
-;;; sort
+			    #:arg-types (list z3:context z3:sort)))
 
+(define z3-mk-int-symbol
+  (foreign-library-function z3-lib "Z3_mk_int_symbol"
+			    #:return-type z3:ast
+			    #:arg-types (list z3:context int)))
 
 
 (define z3-mk-string-symbol
   (foreign-library-function z3-lib "Z3_mk_string_symbol"
-			    #:return-type '*
-			    ;; ctx name sort
-			    #:arg-types '(* * *)))
+			    #:return-type z3:ast
+			    #:arg-types (list z3:context z3:string)))
 
 (define z3-mk-const
   (foreign-library-function z3-lib "Z3_mk_const"
-			    #:return-type '*
+			    #:return-type z3:ast
 			    ;; ctx symbol sort
-			    #:arg-types '(* * *)))
+			    #:arg-types (list z3:context z3:ast z3:sort)))
 
 (define z3-mk-int
   (foreign-library-function z3-lib "Z3_mk_int"
-			    #:return-type '*
+			    #:return-type z3:ast
 			    ;; ctx n sort
-			    #:arg-types (list '* int '*)))
+			    #:arg-types (list z3:context int z3:sort)))
 
 (define z3-mk-divides
   (foreign-library-function z3-lib "Z3_mk_divides"
-			    #:return-type '*
-			    ;;  ctx ast ast
-			    #:arg-types (list '* '* '*)))
+			    #:return-type z3:ast
+			    #:arg-types (list z3:context z3:ast z3:ast)))
 
 (define z3-get-sort
   (foreign-library-function z3-lib "Z3_get_sort"
-			    #:return-type '*
+			    #:return-type z3:sort
 			    ;; ctx ast
-			    #:arg-types '(* *)))
+			    #:arg-types (list z3:context z3:ast)))
 
 
 (define (ast-sort ctx ast)
@@ -119,9 +120,9 @@
 
 (define z3-get-sort-name
   (foreign-library-function z3-lib "Z3_get_sort_name"
-			    #:return-type '*
+			    #:return-type z3:string
 			    ;; ctx sort
-			    #:arg-types '(* *)))
+			    #:arg-types (list z3:context z3:sort)))
 
 ;;;  exported
 ;;;  this is the procedural layer to interact with Z3
